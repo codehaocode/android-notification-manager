7b3be8893b95607c1a1d759cca519ec3fe475e86
Yuhao Zhong <Hao@Yuhaos-MacBook-Pro.local>
2020-09-17 13:22:56 +0200

    Declare and extend NotificationListenerService

---
+++ app/src/main/java/com/github/codehaocode/firstnotificationmanagerapp/NotificationListener.java
@@ -0,0 +1,24 @@
+package com.github.codehaocode.firstnotificationmanagerapp;
+
+import android.content.Intent;
+import android.os.IBinder;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+
+public class NotificationListener extends NotificationListenerService {
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return super.onBind(intent);
+    }
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn){
+
+    }
+
+    @Override
+    public void onNotificationRemoved(StatusBarNotification sbn){
+
+    }
+}
